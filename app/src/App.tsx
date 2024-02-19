7862f3717f9a81ab28ba78e778a9ecc9015638da
DaniIliev <114102749+DaniIliev@users.noreply.github.com>
2024-02-19 20:04:46 +0200

    initial commit

---
+++ app/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+    <div className="app">
+        <div className="main">main</div>
+        <div className="pyramide">money</div>
+    </div>
+    </>
+  )
+}
+
+export default App
