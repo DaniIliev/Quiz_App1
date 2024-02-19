ae4ad1ccd7d26fb8a3e4fdd210e7ae338b01b988
DaniIliev <114102749+DaniIliev@users.noreply.github.com>
2024-02-19 21:55:30 +0200

    create moneyList
    add css

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,12 +1,23 @@
 import { useState } from 'react'
+import MoneyList from './components/shared/MoneyList'
+import Timer from './components/shared/Timer'
+import Quiz from './components/shared/Quiz'
 
 function App() {
 
   return (
     <>
     <div className="app">
-        <div className="main">main</div>
-        <div className="pyramide">money</div>
+        <div className="main">
+          <div className="top">
+            <Timer />
+          </div>
+            {/* <div>Question and answer</div> */}
+            <Quiz /> 
+        </div>
+        <div className="pyramide">
+          <MoneyList />
+        </div>
     </div>
     </>
   )
