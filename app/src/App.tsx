959334cd2a16767c95879c7029c4042c8e4d2941
DaniIliev <114102749+DaniIliev@users.noreply.github.com>
2024-02-20 20:50:21 +0200

    create Quiz component

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -4,7 +4,74 @@ import Timer from './components/shared/Timer'
 import Quiz from './components/shared/Quiz'
 
 function App() {
-
+const data = [
+  {
+    id:1,
+    question: 'Kolko e 1+1',
+    answers:[
+      {
+        text: '4',
+        correct: false,
+      },
+      {
+        text: '3',
+        correct: false,
+      },
+      {
+        text: '2',
+        correct: true,
+      },
+      {
+        text: '5',
+        correct: false,
+      }
+    ]
+  },
+  {
+    id:1,
+    question: 'Kolko e Daniel',
+    answers:[
+      {
+        text: 'pluvec',
+        correct: true,
+      },
+      {
+        text: 'glupak',
+        correct: false,
+      },
+      {
+        text: 'voleibolist',
+        correct: true,
+      },
+      {
+        text: 'Damyan',
+        correct: false,
+      }
+    ]
+  },
+  {
+    id:1,
+    question: 'Kolko e gosho',
+    answers:[
+      {
+        text: 'gosho',
+        correct: true,
+      },
+      {
+        text: 'glupak',
+        correct: false,
+      },
+      {
+        text: 'voleibolist',
+        correct: true,
+      },
+      {
+        text: 'Damyan',
+        correct: false,
+      }
+    ]
+  }
+]
   return (
     <>
     <div className="app">
@@ -12,8 +79,9 @@ function App() {
           <div className="top">
             <Timer />
           </div>
-            {/* <div>Question and answer</div> */}
-            <Quiz /> 
+            <div className="bottom">
+              <Quiz /> 
+            </div>
         </div>
         <div className="pyramide">
           <MoneyList />
---
+++ app/src/components/shared/Quiz.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Quiz = () => {
+  return (
+    <div className='quiz'>
+        <div className="question">Koi e nai krasiviq?</div>
+        <div className="answers">
+            <div className="answer correct">A) Az</div>
+            <div className="answer incorrect">B) Mnogo qsno che az</div>
+            <div className="answer">C) Az e brat koi drug</div>
+            <div className="answer">D) Brat priznai che sym az</div>
+        </div>
+
+    </div>
+  )
+}
+
+export default Quiz
